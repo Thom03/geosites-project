b57b1ca4fa7868570a98582a3448585153b6a9f5
Matthew Hanson <matt.a.hanson@gmail.com>
2016-03-06 17:19:05 -0500

    updated setup sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-PROJECT=`basename $(pwd)`
 DOMAIN=$1
 ROOTPATH=$(pwd)
+PROJECT=`basename $ROOTPATH`
 
 # update project name everywhere and working directory
-find . -type f -exec sed -i "s/{{ project_name }}/$PROJECT/g" {} +
-find . -type f -exec sed -i "s/{{ root_path }}/$ROOTPATH/g" {} +
-find . -type f -exec sed -i "s/{{ domain }}/$DOMAIN/g" {} +
+find . -type f ! -name setup.sh -exec sed -i "s/{{ project_name }}/$PROJECT/g" {} +
+find . -type f ! -name setup.sh -exec sed -i "s|{{ root_path }}|$ROOTPATH|g" {} +
+find . -type f ! -name setup.sh -exec sed -i "s/{{ domain }}/$DOMAIN/g" {} +
 
 mkdir logs
 
