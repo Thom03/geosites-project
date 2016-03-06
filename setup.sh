854050c466ec26dbe287657b43a41ab10454f613
Matthew Hanson <matt.a.hanson@gmail.com>
2016-03-06 16:37:51 -0500

    update domain in setup script

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ ROOTPATH=$(pwd)
 # update project name everywhere and working directory
 find . -type f -exec sed -i 's/{{ project_name }}/$PROJECT/g' {} +
 find . -type f -exec sed -i 's/{{ root_path }}/$ROOTPATH/g' {} +
-find nginx -type f -exec sed -i 's/{{ domain }}/$DOMAIN/g' {} +
+find . -type f -exec sed -i 's/{{ domain }}/$DOMAIN/g' {} +
 
 mkdir logs
 
