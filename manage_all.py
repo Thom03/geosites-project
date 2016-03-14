09d13921b933a54966e7f0cbf35d5b755a11bc5e
Matthew Hanson <matt.a.hanson@gmail.com>
2016-03-14 01:21:47 -0400

    updated manage_all

--- manage_all.py
+++ manage_all.py
@@ -7,13 +7,11 @@ import subprocess
 PROJECT = '{{ project_name }}'
 
 if __name__ == "__main__":
-    from django.core.management import execute_from_command_line
-
     sites = [os.path.basename(d) for d in os.listdir(PROJECT) if os.path.isfile(os.path.join(PROJECT, d, 'settings.py'))]
     for site in sites:
         print 'Managing %s' % site
         path = '%s/%s/settings.py' % (PROJECT, site)
         if os.path.exists(path):
-                os.environ["DJANGO_SETTINGS_MODULE"] = "%s.%s.settings" % (PROJECT, site))
-                # call manage.py with above settings to avoid django caching settings in a single session
-                subprocess.call(['python', 'manage.py'] + sys.argv[1:])
+            os.environ["DJANGO_SETTINGS_MODULE"] = "%s.%s.settings" % (PROJECT, site)
+            # call manage.py with above settings to avoid django caching settings in a single session
+            subprocess.call(['python', 'manage.py'] + sys.argv[1:])
